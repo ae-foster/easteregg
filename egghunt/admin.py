07e5e7132cd5a5b533ff28fbbad54b90b208d332
ae-foster <adamfosterrules@hotmail.co.uk>
2018-09-03 15:51:45 -0700

    Touch ups- explain admin rationale better

--- egghunt/admin.py
+++ egghunt/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Egg, LeaderboardEntry, Image, Downtime
 
 
 admin.site.register(LeaderboardEntry)
+admin.site.register(Downtime)
 
+
+# This allows us to change the images within the egg admin page
 class InlineImage(admin.TabularInline):
     model = Image
     extra = 1
+
 
 class EggAdmin(admin.ModelAdmin):
     inlines = [InlineImage]
 
 
 admin.site.register(Egg, EggAdmin)
-
-admin.site.register(Downtime)
